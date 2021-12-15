560ad32d9b60a404ebdae0d1b4c69fd48b8020a6
ooatamelbug <mohamedwork636@gmail.com>
2021-12-15 14:58:49 +0200

    make entity user

---
+++ src/api/entities/user.ts
@@ -0,0 +1,35 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  BaseEntity,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from "typeorm";
+
+@Entity("users")
+class UserEntity extends BaseEntity {
+  @PrimaryGeneratedColumn("uuid")
+  id: string;
+
+  @Column()
+  username: string;
+
+  @Column()
+  firstname: string;
+
+  @Column()
+  lastname: string;
+
+  @Column()
+  password: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+
+  @UpdateDateColumn()
+  updated_at: Date;
+}
+
+export default UserEntity;
